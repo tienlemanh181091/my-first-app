dabbe3ee8de27bb43181501870aca77c85f08d59
tienlemanh181091 <tien.lemanh.181091@gmail.com>
2025-05-24 10:36:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from CI/CD TEST!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
